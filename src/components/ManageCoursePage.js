0b1091674b1c3c8390e3052b82ad9ee1417437b6
Matthew Liu <mliu78@jh.edu>
2020-06-23 15:28:33 -0400

    'reeee'

---
+++ src/components/ManageCoursePage.js
@@ -0,0 +1,58 @@
+import React, {useState, useEffect} from 'react';
+import CourseForm from './CourseForm';
+import courseStore from '../stores/courseStore';
+import {toast} from 'react-toastify';
+import * as courseActions from '../actions/courseActions';
+
+const ManageCoursePage = (props) => {
+    const [errors, setErrors] = useState({});
+    const [course, setCourse] = useState({
+        id: null,
+        slug: '',
+        title: '',
+        authorId: null,
+        category: ''
+    });
+
+    useEffect(() => {
+        const slug = props.match.params.slug;
+        if(slug) {
+            setCourse(courseStore.getCourseBySlug(slug));
+        }
+    }, [props.match.params.slug])
+
+    function handleChange({target}) {
+        const updatedCourse = {...course, [target.name]: target.value};
+        setCourse(updatedCourse);
+    }
+
+    function formIsValid() {
+        const _errors = {};
+
+        if (!course.title) _errors.title = 'Title is required';
+        if (!course.authorId) _errors.authorId = 'Author ID is required';
+        if (!course.category) _errors.category = 'Category is required';
+
+        setErrors(_errors);
+
+        return Object.keys(_errors).length === 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (!formIsValid()) return;
+        courseActions.saveCourse(course).then(() => {
+            props.history.push('/courses');
+            toast.success('Course saved.');
+        });
+    }
+
+    return (
+        <>
+        <h2>Manage Courses</h2>
+        <CourseForm errors = {errors} course = {course} onChange = {handleChange} onSubmit = {handleSubmit}/>
+        </>
+    )
+}
+
+export default ManageCoursePage;
